2948c508c137ae9d0163c6d8a75b5c23217a70ff
Maciek W <Maciej.Wojcik4@networks.pl>
2018-02-21 15:19:19 +0100

    Add the registration form and fix log in functionality after change from pure SQL to SQLAlchemy

--- project/_config.py
+++ project/_config.py
@@ -4,8 +4,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_SCRF_ENABLED = True
 SECRET_KEY = 'ZM5FqD8WZgF@y%KhpC9X'
 
--- project/views.py
+++ project/views.py
@@ -1,15 +1,23 @@
+###############
+### imports ###
+###############
+
+from forms import AddTaskForm, RegisterForm, LoginForm
+
 from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, \
-    session, url_for, g
-from forms import AddTaskForm
+    session, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-# config
+##############
+### config ###
+##############
+
 app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
-from models import Task
+from models import Task, User
 
 # helper functions
 
@@ -35,16 +43,36 @@ def logout():
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    error = None
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] \
-                or request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid credentials. Please try again.'
-            return render_template('login.html', error=error)
-        else:
-            session['logged_in'] = True
-            flash('Welcome!')
-            return redirect(url_for('tasks'))
-    return render_template('login.html')
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=request.form['name']).first()
+            if user is not None and user.password == request.form['password']:
+                session['logged_in'] = True
+                flash('Welcome!')
+                return redirect(url_for('tasks'))
+            else:
+                error = 'Invalid username or password.'
+    return render_template('login.html', form=form, erro=error)
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                form.name.data,
+                form.email.data,
+                form.password.data
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Thanks for registering. Please log in.')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form, error=error)
 
 
 @app.route('/tasks/')
