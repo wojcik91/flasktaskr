1323184dc43068f80b727f24b28593cf6f28c162
Maciek W <Maciej.Wojcik4@networks.pl>
2018-02-21 12:50:08 +0100

    Add users table to the database

--- project/db_create.py
+++ project/db_create.py
@@ -6,8 +6,8 @@ from datetime import date
 db.create_all()
 
 # insert dummy data into the table
-db.session.add(Task('Finish this tutorial', date(2018, 2, 19), 10, 1))
-db.session.add(Task('Finish Real Python', date(2018, 3, 30), 10, 1))
+#db.session.add(Task('Finish this tutorial', date(2018, 2, 19), 10, 1))
+#db.session.add(Task('Finish Real Python', date(2018, 3, 30), 10, 1))
 
 # commit the changes
 db.session.commit()
