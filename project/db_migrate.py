18002fbd29f31035b481d55b47f5738f10ad0baf
Maciek W <Maciej.Wojcik4@networks.pl>
2018-02-22 09:27:50 +0100

    Add relationship between tasks and users table. Add a database migration script.

---
+++ project/db_migrate.py
@@ -0,0 +1,30 @@
+from views import db
+from _config import DATABASE_PATH
+
+import sqlite3
+from datetime import datetime
+
+with sqlite3.connect(DATABASE_PATH) as connection:
+    # get a cursor object to execute SQL queries
+    c = connection.cursor()
+
+    # temporarily change the name of the tasks table
+    c.execute("""ALTER TABLE tasks RENAME TO old_tasks""")
+
+    # recreate a new tasks table with updated schema
+    db.create_all()
+
+    # retrieve data from old table
+    c.execute("""SELECT name, due_date, priority, status FROM old_tasks
+        ORDER BY task_id ASC""")
+
+    # save all rows as a list of tuples; set posted_date to now, user_id to 1
+    data = [(row[0], row[1], row[2], row[3], datetime.now(), 1)
+            for row in c.fetchall()]
+
+    # insert data into new tasks table
+    c.executemany("""INSERT INTO tasks (name, due_date, priority, status,
+        posted_date, user_id) VALUES (?, ?, ?, ?, ?, ?)""", data)
+
+    # delete old_tasks table
+    c.execute("DROP TABLE old_tasks")
--- project/models.py
+++ project/models.py
@@ -1,5 +1,7 @@
 from views import db
 
+import datetime
+
 
 class Task(db.Model):
 
@@ -9,13 +11,17 @@ class Task(db.Model):
     name = db.Column(db.String, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     priority = db.Column(db.Integer, nullable=False)
+    posted_date = db.Column(db.Date, default=datetime.datetime.utcnow())
     status = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    def __init__(self, name, due_date, priority, status):
+    def __init__(self, name, due_date, priority, posted_date, status, user_id):
         self.name = name
         self.due_date = due_date
         self.priority = priority
+        self.posted_date = posted_date
         self.status = status
+        self.user_id = user_id
 
     def __repr__(self):
         return '<name {0}>'.format(self.name)
@@ -29,6 +35,7 @@ class User(db.Model):
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    tasks = db.relationship('Task', backref='poster')
 
     def __init__(self, name=None, email=None, password=None):
         self.name = name
