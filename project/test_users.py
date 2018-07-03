55b9dc669afe5441038525a62f86b80439e99c76
Wójcik Maciej <MWojcik4@corpo.networks.pl>
2018-07-03 11:27:58 +0200

    Split tests into two files

--- project/test_users.py
+++ project/test_users.py
@@ -64,7 +64,6 @@ class AllTests(unittest.TestCase):
     ### tests ###
     #############
 
-    # each test should start with 'test'
     def test_user_can_register(self):
         new_user = User('michael', 'miachael@mherman.org', 'michaelherman')
         db.session.add(new_user)
@@ -127,65 +126,6 @@ class AllTests(unittest.TestCase):
     def test_not_logged_in_users_cannot_logout(self):
         response = self.logout()
         self.assertNotIn(b'Goodbye!', response.data)
-
-    def test_logged_in_users_can_access_tasks_page(self):
-        self.register('Fletcher', 'fletcher@realpython.com', 'python101', 'python101')
-        self.login('Fletcher', 'python101')
-        response = self.app.get('tasks/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Add a new task:', response.data)
-
-    def test_not_logged_in_users_cannot_access_tasks_page(self):
-        response = self.app.get('tasks/', follow_redirects=True)
-        self.assertIn(b'You need to log in first.', response.data)
-
-    def test_users_can_add_tasks(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        response = self.create_task()
-        self.assertIn(b'New entry was successfully posted.', response.data)
-
-    def test_users_cannot_add_tasks_when_error(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        response = self.app.post('add/', data=dict(
-            name='Go to the bank',
-            due_date='',
-            priority='1',
-            posted_date='2018-06-06',
-            status='1'
-        ), follow_redirects=True)
-        self.assertIn(b'This field is required.', response.data)
-
-    def test_users_can_complete_tasks(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        self.create_task()
-        response = self.app.get('complete/1/', follow_redirects=True)
-        self.assertIn(b'The task was marked as complete.', response.data)
-
-    def test_users_can_delete_tasks(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        self.create_task()
-        response = self.app.get('delete/1/', follow_redirects=True)
-        self.assertIn(b'The task was deleted.', response.data)
-
-    def test_users_cannot_complete_tasks_that_are_not_created_by_them(self):
-        self.create_user('Michael', 'michael@realpython.com', 'python')
-        self.login('Michael', 'python')
-        self.app.get('tasks/', follow_redirects=True)
-        self.create_task()
-        self.logout()
-        self.create_user('Fletcher', 'fletcher@realpython.com', 'python101')
-        self.login('Fletcher', 'python101')
-        self.app.get('tasks/', follow_redirects=True)
-        response = self.app.get('complete/1/', follow_redirects=True)
-        self.assertNotIn(b'The task was marked as complete.', response.data)
 
 
 if __name__ == '__main__':
