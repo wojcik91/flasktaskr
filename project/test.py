2cdb53d58bd4b96e5c9541649dfad7a674659fd8
Wójcik Maciej <MWojcik4@corpo.networks.pl>
2018-07-03 09:47:33 +0200

    Test if users cannot complete tasks not created by them

--- project/test.py
+++ project/test.py
@@ -168,6 +168,18 @@ class AllTests(unittest.TestCase):
         response = self.app.get('delete/1/', follow_redirects=True)
         self.assertIn(b'The task was deleted.', response.data)
 
+    def test_users_cannot_complete_tasks_that_are_not_created_by_them(self):
+        self.create_user('Michael', 'michael@realpython.com', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.create_user('Fletcher', 'fletcher@realpython.com', 'python101')
+        self.login('Fletcher', 'python101')
+        self.app.get('tasks/', follow_redirects=True)
+        response = self.app.get('complete/1/', follow_redirects=True)
+        self.assertNotIn(b'The task was marked as complete.', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
