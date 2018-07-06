f7fa5be36d1f59df1452fc9a0113c301bf86c0f3
Wójcik Maciej <MWojcik4@corpo.networks.pl>
2018-07-06 12:12:08 +0200

    Update the database creation script

--- db_create.py
+++ db_create.py
@@ -1,13 +1,16 @@
-from views import db
-from models import Task
+from project import db
+from project.models import Task, User
 from datetime import date
 
 # create the database and the db table
 db.create_all()
 
 # insert dummy data into the table
-#db.session.add(Task('Finish this tutorial', date(2018, 2, 19), 10, 1))
-#db.session.add(Task('Finish Real Python', date(2018, 3, 30), 10, 1))
+db.session.add(User('admin', 'admin@admin.com', 'admin', 'admin'))
+db.session.add(Task('Finish this tutorial', date(2018, 2, 19), 10,
+                    date(2018, 2, 19), 1, 1))
+db.session.add(Task('Finish Real Python', date(2018, 3, 30), 10,
+                    date(2018, 3, 30), 1, 1))
 
 # commit the changes
 db.session.commit()
