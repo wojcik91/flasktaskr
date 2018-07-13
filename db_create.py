18cf829bc13421ceb0fd66c3a421c152cbf661c9
Wójcik Maciej <MWojcik4@corpo.networks.pl>
2018-07-13 14:40:30 +0200

    Remove incorrect dummy data

--- db_create.py
+++ db_create.py
@@ -5,12 +5,12 @@ from datetime import date
 # create the database and the db table
 db.create_all()
 
-# insert dummy data into the table
+'''# insert dummy data into the table
 db.session.add(User('admin', 'admin@admin.com', 'admin', 'admin'))
 db.session.add(Task('Finish this tutorial', date(2018, 2, 19), 10,
                     date(2018, 2, 19), 1, 1))
 db.session.add(Task('Finish Real Python', date(2018, 3, 30), 10,
-                    date(2018, 3, 30), 1, 1))
+                    date(2018, 3, 30), 1, 1))'''
 
 # commit the changes
 db.session.commit()
