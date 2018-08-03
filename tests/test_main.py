9852ba8d27765f0b55dc0597177a9892ecdb7df3
Maciek W <tapczan666@gmail.com>
2018-08-03 19:06:53 +0200

    Add tests for status codes 404 and 500

---
+++ tests/test_main.py
@@ -0,0 +1,58 @@
+import os
+import unittest
+
+from project import app, db, bcrypt
+from project._config import basedir
+from project.models import User
+
+TEST_DB = 'test.db'
+
+
+class MainTests(unittest.TestCase):
+
+    ##########################
+    #   setup and teardown   #
+    ##########################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQALCHEMY_DATABASE_URI'] = 'sqlite:///' +\
+            os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.create_all()
+
+    # executed after each test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    ######################
+    #   helper methods   #
+    ######################
+    def login(self, name, password):
+        return self.app.post('/', data=dict(name=name, password=password),
+                             follow_redirects=True)
+
+    #############
+    #   tests   #
+    #############
+    def test_404_error(self):
+        response = self.app.get('/this-route-does-not-exist')
+        self.assertEquals(response.status_code, 404)
+
+    def test_500_error(self):
+        bad_user = User(
+            name='Jeremy',
+            email='jeremy@realpython.com',
+            password='django'
+        )
+        db.session.add(bad_user)
+        db.session.commit()
+        response = self.login('Jeremy', 'django')
+        self.assertEquals(response.status_code, 500)
+
+   
+if __name__ == '__main__':
+    unittest.main()
