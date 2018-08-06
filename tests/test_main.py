0b20f270786dbf40a70fc1b28d97326bf300194d
Maciek W <tapczan666@gmail.com>
2018-08-06 14:54:42 +0200

    Test error page content

--- tests/test_main.py
+++ tests/test_main.py
@@ -41,6 +41,7 @@ class MainTests(unittest.TestCase):
     def test_404_error(self):
         response = self.app.get('/this-route-does-not-exist')
         self.assertEquals(response.status_code, 404)
+        self.assertIn(b'Sorry. There\'s nothing here.', response.data)
 
     def test_500_error(self):
         bad_user = User(
@@ -52,6 +53,8 @@ class MainTests(unittest.TestCase):
         db.session.commit()
         response = self.login('Jeremy', 'django')
         self.assertEquals(response.status_code, 500)
+        self.assertNotIn(b'Invalid salt', response.data)
+        self.assertIn(b'Something went terribly wrong', response.data)
 
    
 if __name__ == '__main__':
