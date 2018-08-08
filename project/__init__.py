71d8e52e7f761f00997ccc5bcef848ee6f794aea
Maciek W <tapczan666@gmail.com>
2018-08-08 12:36:28 +0200

    Add error 500 handler and template

--- project/__init__.py
+++ project/__init__.py
@@ -20,3 +20,7 @@ app.register_blueprint(tasks_blueprint)
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
