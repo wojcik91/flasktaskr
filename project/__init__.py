5f6cd68772d2e0931beafca93f2de52a2a8caa79
Maciek W <tapczan666@gmail.com>
2018-08-08 12:19:42 +0200

    Add error handler to app

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 # project/__init__.py
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -15,3 +15,8 @@ from project.tasks.views import tasks_blueprint
 # register blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
